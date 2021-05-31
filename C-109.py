7de00452602308bf5b128e77273fe8ae4f1a68e7
Gurpreetsingh18 <61836288+Gurpreetsingh18@users.noreply.github.com>
2021-05-31 12:12:25 +0530

    Add files via upload

--- C-109.py
+++ C-109.py
@@ -24,12 +24,12 @@ math_first_std_deviation_start, math_first_std_deviation_end = math_mean-math_st
 math_second_std_deviation_start, math_second_std_deviation_end = math_mean-(2*math_std_deviation), math_mean+(2*math_std_deviation)
 math_third_std_deviation_start, math_third_std_deviation_end = math_mean-(3*math_std_deviation), math_mean+(3*math_std_deviation)
 
-#Percentage of data within 1, 2 and 3 Standard Deviations for math
+
 math_list_of_data_within_1_std_deviation = [result for result in math_list if result > math_first_std_deviation_start and result < math_first_std_deviation_end]
 math_list_of_data_within_2_std_deviation = [result for result in math_list if result > math_second_std_deviation_start and result < math_second_std_deviation_end]
 math_list_of_data_within_3_std_deviation = [result for result in math_list if result > math_third_std_deviation_start and result < math_third_std_deviation_end]
 
-#Printing data for math and weight (Standard Deviation)
+
 print("{}% of data for math lies within 1 standard deviation".format(len(math_list_of_data_within_1_std_deviation)*100.0/len(math_list)))
 print("{}% of data for math lies within 2 standard deviations".format(len(math_list_of_data_within_2_std_deviation)*100.0/len(math_list)))
 print("{}% of data for math lies within 3 standard deviations".format(len(math_list_of_data_within_3_std_deviation)*100.0/len(math_list)))
